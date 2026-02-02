ec9094b0a1ce3e8a3062247daa6f0dbe07e19595
diloji99 <diloji99@gmail.com>
2026-02-02 07:56:22 +0000

    Delete Services directory

--- Services/INotificationService.cs
+++
@@ -1,9 +0,0 @@
-﻿// Services/INotificationService.cs
-namespace UniProject.Services
-{
-    public interface INotificationService
-    {
-        Task SendEmailAsync(string toEmail, string subject, string message);
-        Task SendSmsAsync(string toPhoneNumber, string message);
-    }
-}
